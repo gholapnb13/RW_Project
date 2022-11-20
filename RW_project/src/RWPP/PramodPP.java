80063cf809e396c8481fe3f406ef56658cbb012f
nilesh <nileshgholap46@gmail.com>
2022-11-20 10:48:10 +0530

    shear the project

---
+++ RW_project/src/RWPP/PramodPP.java
@@ -0,0 +1,23 @@
+package RWPP;
+
+public class PramodPP {
+	public static void main(String[] args) {
+		
+		int a = 4;
+
+		int b = 8;
+
+		if((b - a--)>= a)
+
+		{
+
+		if((b + a) % 2 == 1)
+
+		System.out.println(a*b); else
+
+		System.out.println(b + a);
+
+	}
+	}
+
+}
